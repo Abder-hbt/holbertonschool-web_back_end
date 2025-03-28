25c0e06f3ce1990a44aff524644144cafbf1006a
Abder-hbt <9907@holbertonstudents.com>
2025-03-28 23:55:24 +0100

    Update 12-createReportObject.js

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,1 +1,8 @@
-s
+export default function createReportObject(employeesList) {
+  return {
+    allEmployees: employeesList,
+    getNumberOfDepartments() {
+      return Object.keys(this.allEmployees).length;
+    },
+  };
+}
