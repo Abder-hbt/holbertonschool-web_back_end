9a557dd5be603070702df127c5c966c9efd90bee
Abder-hbt <9907@holbertonstudents.com>
2025-03-28 23:54:46 +0100

    Update 10-loops.js

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,1 +1,8 @@
-s
+export default function appendToEachArrayValue(array, appendString) {
+  const theNewA = [];
+  for (const string of array) {
+    theNewA.push(appendString + string);
+  }
+
+  return theNewA;
+}
