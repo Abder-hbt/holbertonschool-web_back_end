c77dc970ab2c68879ffb4b4d9535ce649715a88f
Abder-hbt <9907@holbertonstudents.com>
2025-03-31 13:49:36 +0000

    SUII

---
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,7 @@
+export default function getStudentsByLocation (student, city) {
+    if (!Array.isArray(student)){
+        return []
+    }
+
+    return student.filter(student => student.location === city)
+}
