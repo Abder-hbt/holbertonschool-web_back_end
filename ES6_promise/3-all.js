6b4381dfd7051d4e98178dfd82ea8e11297909de
Abder-hbt <9907@holbertonstudents.com>
2025-04-04 15:28:59 +0000

    SUII

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,12 @@
+import { uploadPhoto, createUser } from './utils/utils.js';
+
+
+export default function handleProfileSignup(){
+    Promise.all([uploadPhoto(), createUser()])
+        .then(([photoResult, userResult]) => {
+            console.log(`${photoResult.body} ${userResult.firstName} ${userResult.lastName}`);
+        })
+        .catch((error) => {
+            console.log('Signup system offline');
+    });
+}
