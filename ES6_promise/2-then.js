a97ee0ccf964a78b84dc455ca60a04aaa2ea5fb4
Abder-hbt <9907@holbertonstudents.com>
2025-04-04 14:50:54 +0000

    SUII

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,13 +1,9 @@
 export default function handleResponseFromAPI(promise){
     
     return promise
-        .then((message) => {
+        .then(() => ({ status: 200, body: 'success' }))
+        .catch(()=> new Error())
+        .finally(() => {
             console.log("Got a response from the API");
-            return { status: 200, body: 'success' };
-            
-        })
-        .catch((message)=> {
-            console.log("Got a response from the API");
-            throw new Error('An error occurred');
         });
 }
