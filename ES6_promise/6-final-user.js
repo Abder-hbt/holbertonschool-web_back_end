51d66da9f7b15f1a146ea2f29b7dd91de846274a
Abder-hbt <9907@holbertonstudents.com>
2025-04-07 11:40:25 +0000

    SUII

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -7,8 +7,4 @@ export default function handleProfileSignup (firstName, lastName, fileName){
     const signUpPromise = signUpUser(firstName, lastName);
     const uploadPromise = uploadPhoto(fileName)
     return Promise.allSettled([uploadPromise, signUpPromise])
-        .then((results) => results.map((result) => ({
-            status: result.status,
-            value: result.status === 'fulfilled' ? result.value : result.reason.toString(),
-        })));
 }
