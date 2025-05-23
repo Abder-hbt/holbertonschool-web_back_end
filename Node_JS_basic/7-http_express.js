c98ae8c3963fd1cd5ff66df1e9f1cdfc98f23cef
Abder-hbt <9907@holbertonstudents.com>
2025-05-23 23:56:30 +0200

    Update 7-http_express.js

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -2,19 +2,15 @@ const express = require('express');
 const countStudents = require('./3-read_file_async');
 
 const app = express();
-const port = 1245;
-const hostname = '127.0.0.1';
-const filePath = process.argv[2];
-const promise = countStudents(filePath);
-
-app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
+app.get('/', (req, res) => res.send('Hello Holberton School!'));
+app.get('/students', async (req, res) => {
+  const title = 'This is the list of our students\n';
+  try {
+    const data = await countStudents(process.argv[2]);
+    res.send(`${title}${data.join('\n')}`);
+  } catch (error) {
+    res.send(`${title}${error.message}`);
+  }
 });
-
-app.get('/students', (req, rep) => {
-  promise.then((data) => {
-    rep.send(`This is the list of our students\n${data}`);
-  }).catch(() => { rep.send('This is the list of our students\nCannot load the database'); });
-});
-
-module.exports = app.listen(port, hostname);
+app.listen(1245);
+module.exports = app;
